39624c39a01b43e7e0fde3dd44c25de4b814cb74
Haley C. King <haleyck3@gmail.com>
2016-05-09 20:59:37 -0500

    add bootstrap

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,16 @@ class WelcomeController < ApplicationController
   def index
   end
 
-  def test
+	def test
+    response = HTTParty.get("http://api.wunderground.com/api/b429b7a5875cd22e/geolookup/conditions/q/TX/Dallas.json")
+  
+    @location = response['location']['city']
+    @temp_f = response['current_observation']['temp_f']
+    @temp_c = response['current_observation']['temp_c']
+    @weather_icon = response['current_observation']['icon_url']
+    @weather_words = response['current_observation']['weather']
+    @forecast_link = response['current_observation']['forecast_url']
+    @real_feel_f = response['current_observation']['feelslike_f']
+    @real_feel_c = response['current_observation']['feelslike_c']
   end
 end
